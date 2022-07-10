013b4bcdc09d1d81c893cf34bed10685e1b7e041
NyanC0de <104165638+NyanC0de@users.noreply.github.com>
2022-07-10 18:12:21 +0200

    Update and rename login.html to login.php

--- login.php
+++ login.php
@@ -7,8 +7,9 @@
   <body>
     <a href="https://nyanc0de.github.io/NyanShorts/"><img src="kitten.png" class="logo"></a>
     <h1 class="email_text">E-Mail</h1>
-    <input class="email" type="text" value="">
+    <input class="email" type="text" name="email" value="">
     <h1 class="password_text">Password</h1>
-    <input class="password" type="password" value="">
+    <input class="password" type="password"  name="password" value="">
+    <button class="login_button" type="submit">Log in</button>
   </body>
 </html>
